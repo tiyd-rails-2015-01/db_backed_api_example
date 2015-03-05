cd5b66b431a5b618eb1136becfaa0acb55a5533d
Topstitch <kallen@spoonflower.com>
2015-03-04 21:35:00 -0500

    Seems like the profile update is working

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -22,8 +22,37 @@ class Profile < ActiveRecord::Base
     end
   end
 
-  def self.decide_which_one(username)
-    Profile.find_by_username(username) ||
-      Profile.create_from_username(username)
+  def update_from_api
+    response = HTTParty.get(
+        "https://api.github.com/users/#{self.username}",
+        :headers => {"Authorization" => "token #{ENV['GITHUB_TOKEN']}",
+                     "User-Agent" => "anyone"
+                    }
+    )
+    if response["login"]
+      self.update(body: response,
+                    username: response["login"],
+                    avatar_url: response["avatar_url"],
+                    location: response["location"],
+                    company_name: response["company"],
+                    number_of_followers: response["followers"],
+                    number_following: response["following"]
+    )
+    else
+      return nil
+    end
   end
+
+  def need_to_update?
+    self.updated_at < 1.minute.ago
+  end
+
+  def self.decide_which_one(username)
+    p = Profile.find_by_username(username) ||
+      Profile.create_from_username(username)
+
+    p.update_from_api if p.need_to_update?
+    return p
+  end
+
 end
